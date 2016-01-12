976a85c1a9fb7dac1ef4274ebb28ba098a6edf87
Solomin Andrey <solomin.andrey@gmail.com>
2016-01-12 17:20:40 +0300

    Конструктор без параметров создает безымянную кошку

--- src/ru/solandme/geekbrains/lesson2/Main.java
+++ src/ru/solandme/geekbrains/lesson2/Main.java
@@ -3,8 +3,8 @@ package ru.solandme.geekbrains.lesson2;
 public class Main {
     public static void main(String[] args) {
 
-        Cat cat1 = new Cat(3, "Murka", false);
-        Cat cat2 = new Cat(2, "Tom", true);
+        Cat cat1 = new Cat(3, "Мурка", false);
+        Cat cat2 = new Cat(2, "Том", true);
         Cat cat3 = new Cat();
 
         cat1.about();
